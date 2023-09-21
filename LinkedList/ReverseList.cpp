d9c39bbe5a8f65b8a7fb969f86a0bd1ba54241e0
CharandeepSingh01 <login.charandeep@gmail.com>
2023-09-21 14:13:06 +0530

    added

---
+++ LinkedList/ReverseList.cpp
@@ -0,0 +1,9 @@
+    ListNode* rev(ListNode* head){
+        ListNode* dummy=NULL;
+        while(head!=NULL){
+            ListNode* temp=head->next;
+            head->next=dummy;
+            dummy=head;
+            head=temp;
+        }
+    }
