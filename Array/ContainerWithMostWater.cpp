05d0ec079d6c123b5a28968ba0834b6f419aeda9
CharandeepSingh01 <login.charandeep@gmail.com>
2023-10-01 11:47:10 +0530

    added

---
+++ Array/ContainerWithMostWater.cpp
@@ -0,0 +1,17 @@
+int maxArea(vector<int>& height) {
+int area=0;
+    int n=height.size();
+    int i=0;
+    int j=n-1;
+    while(i<j){
+        int h=min(height[i],height[j]);
+        area=max(area,h*(j-i));
+        if(height[i]<height[j]){
+            i++;
+        }
+        else{
+            j--;
+        }
+    }
+    return area;
+}
