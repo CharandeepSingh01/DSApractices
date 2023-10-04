d850610c15fc6057a636741f0eaef5107e545ee7
CharandeepSingh01 <login.charandeep@gmail.com>
2023-10-04 23:53:56 +0530

    added

---
+++ LinkedList/RotateListByKtimes.cpp
@@ -0,0 +1,12 @@
+ListNode* rotateRight(ListNode* head, int k) {
+    if (!head || !head->next || k == 0) return head;
+    ListNode *cur = head;
+    int len = 1;
+    while (cur->next && ++len) cur = cur->next;
+    cur->next = head;
+    k = len - k % len;
+    while (k--) cur = cur->next;
+    head = cur->next;
+    cur->next = nullptr;
+   return head; 
+}
